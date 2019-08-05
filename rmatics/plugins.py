ec2c7abbc38bbadfb270fb3c00642e43bf750159
Ilya Rusnen <ilya@rusanen.co.uk>
2019-08-05 15:17:49 +0300

    feat (NFRMTCS-192): Allow monitor cacher to use context

--- rmatics/plugins.py
+++ rmatics/plugins.py
@@ -5,4 +5,5 @@ invalidator = MonitorCacheInvalidator(autocommit=False)
 
 monitor_cacher = FlaskCacher(prefix='monitor', cache_invalidator=invalidator,
                              allowed_kwargs=['problem_id', 'user_ids',
-                                             'time_after', 'time_before'])
+                                             'time_after', 'time_before'
+                                             'context_id', 'context_source', 'show_hidden'])
