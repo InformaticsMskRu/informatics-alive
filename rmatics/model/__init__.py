383647c744009c23b56310ed48fd4f5d6917f084
semkiku <82077476+Semen-prog@users.noreply.github.com>
2026-07-09 22:08:25 +0300

    Revert "delete unneeded"
    This reverts commit f89b9fa390876add250e52d238883940f459b2f4.

--- rmatics/model/__init__.py
+++ rmatics/model/__init__.py
@@ -1,8 +1,11 @@
+from rmatics.model.comment import Comment
 from rmatics.model.course import Course
 from rmatics.model.course_module import CourseModule
 from rmatics.model.course_section import CourseSection
 from rmatics.model.ejudge_contest import EjudgeContest
+from rmatics.model.ejudge_run import EjudgeRun
 from rmatics.model.group import Group, UserGroup
+from rmatics.model.ideal_solution import Ideal
 from rmatics.model.monitor import MonitorCourseModule, Monitor
 from rmatics.model.problem import (
     EjudgeProblem,
@@ -10,6 +13,7 @@ from rmatics.model.problem import (
 )
 
 from rmatics.model.run import Run
+from rmatics.model.standings import ProblemStandings, StatementStandings
 from rmatics.model.stars import Stars
 from rmatics.model.statement import Statement, StatementUser, StatementProblem
 from rmatics.model.user import SimpleUser, User, PynformaticsUser
